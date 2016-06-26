5adf0ccf258eb7548ddae5aec26764380960b06a
traducer <traducer21@gmail.com>
2016-06-26 03:35:14 +0000

    refactored some route logic, created table schema creationg via knex, tables are created prior to starting the server using promises

--- backend/config/todo.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS todos (
-  id SERIAL,
-  task TEXT,
-  completed BOOLEAN,
-  importance TEXT,
-  created_at TIMESTAMP,
-  updated_at TIMESTAMP
-);
--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,26 @@ const methodOverride = require('method-override')
 const morgan = require('morgan')
 const todoRoutes = require('./routes/todo')
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(methodOverride('_method'))
-app.use(morgan('dev'))
+/* importing database to createTable if it doesn't exist
+ * start our application after database tables have been created */
 
-app.use('/api/todos', todoRoutes)
+const db = require('./config/database')
 
-app.listen(process.env.PORT || 3000, process.env.IP, function () {
-  console.log('ToDo api server has started.')
-})
+db.createTables()
+  .then(() => {
+    console.log('tables have been created')
+
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(methodOverride('_method'))
+    app.use(morgan('dev'))
+
+    app.use('/api/todos', todoRoutes)
+
+    app.listen(process.env.PORT || 3000, process.env.IP, function () {
+      console.log('ToDo api server has started.')
+    })
+  })
+  .catch(err => {
+    console.error(err.stack)
+  })
+
