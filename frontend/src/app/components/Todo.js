504e8b65cf73ebed1452ea9f339a14ead8988cf6
traducer <traducer21@gmail.com>
2016-06-27 18:15:21 +0000

    added delete button and proptypes to Todo component

--- frontend/src/app/components/Todo.js
+++ frontend/src/app/components/Todo.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 
-export default ({ task, importance, completed, id }) => {
+const Todo = ({ task, importance, completed, id, deleteTodo }) => {
   return (
     <li className='todo-item' data-id={id} data-importance={importance}>
       {task}
+      <button onClick={() => deleteTodo(id)}>delete</button>
     </li>
   )
 }
+
+Todo.propTypes = {
+  task: PropTypes.string,
+  importance: PropTypes.string,
+  completed: PropTypes.bool,
+  id: PropTypes.number,
+  deleteTodo: PropTypes.func
+}
+
+export default Todo
