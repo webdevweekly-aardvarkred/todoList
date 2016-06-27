bb9ee096e86fcce2ff1a72c9ef99c4d2f8d65df3
traducer <traducer21@gmail.com>
2016-06-27 18:14:00 +0000

    added delete todo actions

--- frontend/src/app/actions/index.js
+++ frontend/src/app/actions/index.js
@@ -4,6 +4,10 @@ function addTodo (payload) {
   return axios.post('/api/todos', payload)
 }
 
+function deleteTodo (id) {
+  return axios.delete(`/api/todos/${id}`)
+}
+
 export const ADD_TODO = ({ id, task, completed, importance }) => ({
   type: 'ADD_TODO',
   id,
@@ -12,12 +16,31 @@ export const ADD_TODO = ({ id, task, completed, importance }) => ({
   importance
 })
 
+export const DELETE_TODO = (id) => ({
+  type: 'DELETE_TODO',
+  id
+})
+
 export const ATTEMPT_ADD = (payload) => {
   return function (dispatch) {
     return addTodo(payload)
       .then(response => {
         if (response.status === 200) {
-          return dispatch(ADD_TODO(payload))
+          return dispatch(ADD_TODO(response.data.data))
+        }
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+}
+
+export const ATTEMPT_DELETE = (id) => {
+  return function (dispatch) {
+    return deleteTodo(id)
+      .then(response => {
+        if (response.status === 200) {
+          return dispatch(DELETE_TODO(id))
         }
       })
       .catch(err => {
