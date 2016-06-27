eb3065e7fd72071150a3933130da34acb28abd25
traducer <traducer21@gmail.com>
2016-06-27 08:29:06 +0000

    removed todos state from TodoInputContainer, its not used

--- frontend/src/app/containers/TodoInputContainer.js
+++ frontend/src/app/containers/TodoInputContainer.js
@@ -2,11 +2,7 @@ import { connect } from 'react-redux'
 import { ATTEMPT_ADD } from '../actions'
 import TodoInput from '../components/TodoInput'
 
-const mapStateToProps = (state) => ({
-  todos: state.todos
-})
-
 export default connect(
-  mapStateToProps,
+  null,
   { ATTEMPT_ADD }
 )(TodoInput)
